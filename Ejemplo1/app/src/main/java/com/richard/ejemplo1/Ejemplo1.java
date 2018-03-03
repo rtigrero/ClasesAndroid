1b422460d5231bb5b95264697716a433bcef8e4a
rtigrero <32083244+rtigrero@users.noreply.github.com>
2018-03-03 11:20:59 -0500

    version 1 ejemplo1
    version 1.1.0

---
+++ Ejemplo1/app/src/main/java/com/richard/ejemplo1/Ejemplo1.java
@@ -0,0 +1,13 @@
+package com.richard.ejemplo1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Ejemplo1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ejemplo1);
+    }
+}
